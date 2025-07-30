dd3bdbfea19aef7bfc274b62af927414cd0df024
Parsa <devparand@gmail.com>
2025-07-30 19:56:55 +0330

    Initial Commit

---
+++ SibLearn/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SibLearn
+//
+//  Created by Parsa on 7/30/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ SibLearnTests/SibLearnTests.swift
@@ -0,0 +1,17 @@
+//
+//  SibLearnTests.swift
+//  SibLearnTests
+//
+//  Created by Parsa on 7/30/25.
+//
+
+import Testing
+@testable import SibLearn
+
+struct SibLearnTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
